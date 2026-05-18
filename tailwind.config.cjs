5cd02ed51dc69b727b16008f5d1852593e546c43
Darko Stankovski <darko.itpro@gmail.com>
2026-05-18 09:03:56 +0200

    Updated styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,12 +7,6 @@ module.exports = {
       fontFamily: {
         mono: ['"JetBrains Mono"', '"Fira Code"', 'Consolas', 'monospace'],
       },
-      colors: {
-        amber: {
-          500: '#EF9F27',
-          600: '#BA7517',
-        },
-      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
